0e5eb7bfbcb0730bc6306c0a7b1f3a3ad146b96a
Achim <cadarachim@gmail.com>
2024-06-01 20:02:45 +0300

    created loop for animation

--- My bunny.py
+++ My bunny.py
@@ -1,30 +1,37 @@
 import turtle
+import time
 
-t = turtle.Turtle()
-turtle.title("For My Bunny")
+def draw_heart():
+    t = turtle.Turtle()
+    t.color("red")
+    t.fillcolor("red")
+    t.begin_fill()
 
-screen=turtle.Screen()
+    t.left(140)
+    t.forward(180)
+    t.circle(-90, 200)
+    t.setheading(60)
+    t.circle(-90, 200)
+    t.forward(180)
+
+    t.end_fill()
+
+    t.up()
+    t.setpos(-65, 150)
+    t.down()
+    t.color('lightgreen')
+    t.write("I LOVE YOU", font=("Verdana", 14, "bold"))
+
+    t.ht()
+
+# Setup screen
+screen = turtle.Screen()
+screen.title("For My Bunny")
 screen.bgcolor("black")
 
-t.color("red")
-t.fillcolor("red")
-t.begin_fill()
-
-t.left(140)
-t.forward(180)
-t.circle(-90, 200)
-t.setheading(60)
-t.circle(-90, 200)
-t.forward(180)
-
-t.end_fill()
-
-t.up()
-t.setpos(-65, 150)
-t.down()
-t.color('lightgreen')
-t.write("I LOVE YOU", font=("Verdana", 14, "bold"))
-
-t.ht()
-
-turtle.mainloop()
+# Run the heart drawing in a loop
+while True:
+    draw_heart()
+    time.sleep(1)
+    screen.clear()  # Clear the screen to draw the heart again
+    screen.bgcolor("black") 
